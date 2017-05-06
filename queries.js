56211908b2138e5bb8d054acf66a36b90fab2739
moinkhan7402 <khan.moin7402@gmail.com>
2017-05-06 14:37:18 +0530

    initial commit

---
+++ queries.js
@@ -0,0 +1,35 @@
+﻿var promise = require('bluebird');
+
+var options = {
+    // Initialization Options
+    promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://postgres:secure@1234@localhost:8080/MBC';
+var db = pgp(connectionString);
+
+
+function getAllTask(req, res, next) {
+    debugger;
+    db.any('select * from tasklist')
+        .then(function (data) {
+          
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Retrieved ALL puppies',
+                    data: data
+                    
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+
+
+module.exports = {
+    getAllTask: getAllTask,
+};
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+
+var express = require('express');
+var router = express.Router();
+var db = require('../queries');
+
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api/tasklist', db.getAllTask);
+
+module.exports = router;
